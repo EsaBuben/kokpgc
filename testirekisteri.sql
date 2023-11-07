7c75b0707ac341a34612b568ca48d8db6d1750a3
Esa Buben <116266185+EsaBuben@users.noreply.github.com>
2023-11-07 10:54:44 +0200

    Update testirekisteri.sql

--- testirekisteri.sql
+++ testirekisteri.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 07, 2023 at 09:25 AM
+-- Generation Time: Nov 07, 2023 at 09:52 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -32,6 +32,31 @@ CREATE TABLE `project` (
   `Project_Name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projectrole`
+--
+
+CREATE TABLE `projectrole` (
+  `ref_project_id` int(11) NOT NULL,
+  `ref_user_id` int(11) NOT NULL,
+  `role` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `responsible` varchar(100) DEFAULT NULL,
+  `profile_name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
@@ -43,6 +68,19 @@ ALTER TABLE `project`
   ADD PRIMARY KEY (`Project_ID`);
 
 --
+-- Indexes for table `projectrole`
+--
+ALTER TABLE `projectrole`
+  ADD PRIMARY KEY (`ref_project_id`,`ref_user_id`),
+  ADD KEY `ref_user_id` (`ref_user_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -51,8 +89,26 @@ ALTER TABLE `project`
 --
 ALTER TABLE `project`
   MODIFY `Project_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `projectrole`
+--
+ALTER TABLE `projectrole`
+  ADD CONSTRAINT `projectrole_ibfk_1` FOREIGN KEY (`ref_project_id`) REFERENCES `project` (`Project_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `projectrole_ibfk_2` FOREIGN KEY (`ref_user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
