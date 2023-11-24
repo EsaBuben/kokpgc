5fd275b1cf2575c0e8a76c5fa5d2b08fdf2af6ac
simokuos <simo.kuosmanen@student.lab.fi>
2023-11-24 09:56:54 +0200

    test.cs main attributes, getter&setter done

--- TestIt/TestIt/Test.cs
+++ TestIt/TestIt/Test.cs
@@ -3,12 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
-using System.Data.SqlClient;
 
 namespace TestIt
 {
-    internal class Test
+    public class Test
     {
+      private int testID;
+      private string name;
+      //private Status status; // int for id ?
+      private int priority;
+      //private string instructions;
+      private User resp_user;
+      //private Result result;
+      public Test(){
+        this.name = "No name was given";
+        this.priority = -1;
+      }
+
+      public Test(string name):base(){
+        this.name = name;
+      }
+
+      public Test(string name, int priority){
+        this.name = name;
+        this.priority = priority;
+      }
+
+      public int ID{
+        get{return this.testID;}
+        set{this.testID = value;}
+      }
+
+      public string Name{
+        get{return this.name;}
+        set{this.name = value;}
+      }
+
+      public int Priority{
+        get{return this.priority;}
+        set{this.priority = value;}
+      }
+
+      public User Responsible_user{
+        get{return this.resp_user;}
+        set{this.resp_user = value;}
+      }
+
     }
+
 }
