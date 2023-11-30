edf8e0b527eba9ba8ac90c1479a168dbdd418555
simokuos <simo.kuosmanen@student.lab.fi>
2023-11-30 08:48:45 +0200

    GatewayConstruct

--- TestIt/TestIt/Controller.cs
+++ TestIt/TestIt/Controller.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Navigation;
+//using System.Windows.Navigation;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace TestIt
@@ -23,59 +23,23 @@ namespace TestIt
     {
         public static Object AddNew(Object obj, DataObjectType type)
         {
-            IGateway gateway;
-            switch(type)
-            {
-                case DataObjectType.Project:
-                    gateway = new ProjectGateway();
-                    gateway.Insert(obj);
-                    break;
-                case DataObjectType.Test:
-                    gateway = new TestGateway();
-                    gateway.Insert(obj);
-                    break;
-                case DataObjectType.Functionality:
-                    gateway = new FunctionalityGateway();
-                    gateway.Insert(obj);
-                    break;
-                default:
-                break;
-            }
-            return obj;
+            GatewayConstructor gateway = new GatewayConstructor(type);
 
-        }
-        public static Object Listaa(int id, DataObjectType type)
-        {
-            IGateway gateway;
-            switch (type)
-            {
-                case DataObjectType.Test:
-                    gateway = new TestGateway();
-                    return gateway.SelectAll();
-                case DataObjectType.Functionality:
-                    gateway = new FunctionalityGateway();
-                    return gateway.SelectAll(id);
-                default:
-                    break;
-            }
-            return null;
+            gateway.Insert(obj);
+
+            return obj;
 
         }
         public static List<Object> Listaa(DataObjectType type)
         {
-            IGateway gateway;
-            switch (type)
-            {
-                case DataObjectType.Project:
-                    gateway = new ProjectGateway();
-                    return gateway.SelectAll();
-                case DataObjectType.Test:
-                    gateway = new TestGateway();
-                    return gateway.SelectAll();
-                default:
-                    break;
-            }
-            return null;
+            GatewayConstructor gateway = new GatewayConstructor(type);
+            return gateway.SelectAll();
+
+        }
+        public static Object Listaa(int id, DataObjectType type)
+        {
+          GatewayConstructor gateway = new GatewayConstructor(type);
+          return gateway.SelectAll(id);
 
         }
 
--- TestIt/TestIt/GatewayConstructor.cs
+++ TestIt/TestIt/GatewayConstructor.cs
@@ -1,46 +1,49 @@
-// ﻿using System;
-//
-// namespace TestIt
-// {
-//     public class GatewayConstructor:IGateway
-//     {
-//         private IGateway gateway;
-//         public GatewayConstructor(DataObjectType dtype)
-//         {
-//         switch(dtype)
-//           {
-//           case DataObjectType.Project:
-//             gateway = new ProjectGateway();
-//             break;
-//           case DataObjectType.Funktionality:
-//             gateway = new FunctionalityGateway();
-//             break;
-//           case DataObjectType.Test:
-//             gateway = new TestGateway();
-//             break;
-//           case DataObjectType.Result:
-//             break;
-//           case DataObjectType.User:
-//             break;
-//
-//           }
-//         }
-//
-//         public void Insert(obj){
-//           gateway.Insert(obj);
-//         }
-//
-//         public List<Object> SelectAll(){
-//           return gateway.SelectAll();
-//         }
-//
-//         public List<Object> SelectAll(int id){
-//           return gateway.SelectAll(int id);
-//         }
-//
-//
-//     }
-//
-//
-//
-// }
+﻿using System;
+using System.Collections.Generic;
+namespace TestIt
+{
+    public class GatewayConstructor:IGateway
+    {
+        private IGateway gateway;
+        public GatewayConstructor(DataObjectType dtype)
+        {
+        switch(dtype)
+          {
+          case DataObjectType.Project:
+            this.gateway = new ProjectGateway();
+            break;
+          case DataObjectType.Functionality:
+            this.gateway = new FunctionalityGateway();
+            break;
+          case DataObjectType.Test:
+            this.gateway = new TestGateway();
+            break;
+          case DataObjectType.Result:
+            break;
+          case DataObjectType.User:
+            break;
+          default:
+            this.gateway = null;
+            break;
+          }
+      
+        }
+
+        public void Insert(Object obj){
+          this.gateway.Insert(obj);
+        }
+
+        public List<Object> SelectAll(){
+          return gateway.SelectAll();
+        }
+
+        public List<Object> SelectAll(int id){
+          return gateway.SelectAll(id);
+        }
+
+
+    }
+
+
+
+}
