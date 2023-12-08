4c18b82db60143f91e3a5e9cdd0f9874beab786f
Esa Buben <116266185+EsaBuben@users.noreply.github.com>
2023-12-08 09:10:31 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- TestIt/TestIt/Viewer.cs
+++ TestIt/TestIt/Viewer.cs
@@ -35,6 +35,7 @@ namespace TestIt
             if (curry == DataObjectType.Functionality)
             {
                 otsikko.Text = curry.ToString();
+                addBox.Text = "Add " + curry.ToString();
                 bindingSource.DataSource = Controller.Listaa(valitutPalat[(int)curry], curry);
                 Taulukko.DataSource = bindingSource;
                 userStory.Visible = true;
@@ -42,6 +43,7 @@ namespace TestIt
             else
             {
                 otsikko.Text = curry.ToString();
+                addBox.Text = "Add " + curry.ToString();
                 userStory.Visible = false;
                 bindingSource.DataSource = Controller.Listaa(curry);
                 Taulukko.DataSource = bindingSource;
@@ -60,6 +62,7 @@ namespace TestIt
                 valitutPalat[(int)curry] = (int)Taulukko[1, e.RowIndex].Value;
                 bindingSource.DataSource = Controller.Listaa(valitutPalat[(int)curry], curry);
                 otsikko.Text = curry.ToString();
+                addBox.Text = "Add " + curry.ToString();
                 userStory.Visible = false;
                 if (curry == DataObjectType.Functionality)
                 {
